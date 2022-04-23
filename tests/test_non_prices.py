aeef3d115ed0baf826eaee659bfaf066ec6faa6e
mberk <maurice@mauriceberk.com>
2022-04-23 07:29:59 +0100

    Add unit tests

---
+++ tests/test_non_prices.py
@@ -0,0 +1,575 @@
+import json
+from copy import deepcopy
+from pathlib import Path
+from typing import Any, Dict
+
+import pandas as pd
+import pytest
+import smart_open
+from betfairlightweight.resources import MarketBook
+from betfairlightweight.resources import MarketCatalogue
+from betfairlightweight.resources import MarketDefinition
+from betfairlightweight.resources import RunnerBook
+
+from betfairutil import calculate_book_percentage
+from betfairutil import calculate_market_book_diff
+from betfairutil import calculate_total_matched
+from betfairutil import does_market_book_contain_runner_names
+from betfairutil import does_market_definition_contain_runner_names
+from betfairutil import EX_KEYS
+from betfairutil import filter_runners
+from betfairutil import get_market_id_from_string
+from betfairutil import get_race_id_from_string
+from betfairutil import get_runner_book_from_market_book
+from betfairutil import get_selection_id_to_runner_name_map_from_market_catalogue
+from betfairutil import get_win_market_id_from_race_card
+from betfairutil import is_market_book
+from betfairutil import is_runner_book
+from betfairutil import iterate_other_active_runners
+from betfairutil import market_book_to_data_frame
+from betfairutil import prices_file_to_csv_file
+from betfairutil import random_from_market_id
+from betfairutil import read_prices_file
+from betfairutil import read_race_file
+from betfairutil import remove_bet_from_runner_book
+from betfairutil import Side
+
+
+@pytest.fixture
+def race_card():
+    return {
+        "race": {
+            "markets": [
+                {"marketId": "924.123", "marketType": "WIN", "numberOfWinners": 1},
+                {"marketId": "927.123", "marketType": "WIN", "numberOfWinners": 1},
+                {"marketId": "1.123", "marketType": "PLACE", "numberOfWinners": 3},
+                {"marketId": "1.456", "marketType": "WIN", "numberOfWinners": 1},
+            ]
+        }
+    }
+
+
+@pytest.fixture
+def market_catalogue():
+    return {
+        "runners": [
+            {
+                "selectionId": 123,
+                "runnerName": "foo",
+                "sortPriority": 1,
+                "status": "ACTIVE",
+            },
+            {
+                "selectionId": 456,
+                "runnerName": "bar",
+                "sortPriority": 2,
+                "status": "ACTIVE",
+            },
+        ]
+    }
+
+
+@pytest.fixture
+def market_definition():
+    return {
+        "runners": [
+            {"id": 123, "name": "foo", "sortPriority": 1, "status": "ACTIVE"},
+            {"id": 456, "name": "bar", "sortPriority": 2, "status": "ACTIVE"},
+        ],
+        "betDelay": 5,
+        "bettingType": "ODDS",
+        "bspMarket": False,
+        "bspReconciled": False,
+        "complete": True,
+        "crossMatching": True,
+        "discountAllowed": True,
+        "eventId": "789",
+        "inPlay": True,
+        "marketBaseRate": 5,
+        "marketTime": "2022-04-03T14:00:00.000Z",
+        "eventTypeId": "2",
+        "numberOfActiveRunners": 2,
+        "numberOfWinners": 1,
+        "persistenceEnabled": True,
+        "regulators": ["MR_INT"],
+        "runnersVoidable": False,
+        "status": "OPEN",
+        "timezone": "GMT",
+        "turnInPlayEnabled": True,
+        "version": 123456789,
+        "marketType": "MATCH_ODDS",
+    }
+
+
+@pytest.fixture
+def market_book(market_definition: Dict[str, Any]):
+    return {
+        "runners": [
+            {
+                "selectionId": 123,
+                "status": "ACTIVE",
+                "handicap": 0.0,
+                "ex": {
+                    "availableToBack": [{"price": 1.98, "size": 1}],
+                    "availableToLay": [],
+                    "tradedVolume": [],
+                },
+            },
+            {
+                "selectionId": 456,
+                "status": "ACTIVE",
+                "handicap": 0.0,
+                "ex": {
+                    "availableToBack": [{"price": 1.98, "size": 1}],
+                    "availableToLay": [],
+                    "tradedVolume": [],
+                },
+            },
+        ],
+        "marketDefinition": market_definition,
+        "marketId": "1.123",
+        "inplay": True,
+        "publishTime": 1648994400000,
+    }
+
+
+def test_side():
+    assert Side.LAY.other_side == Side.BACK
+
+
+@pytest.mark.parametrize("use_market_book_objects", [False, True])
+def test_calculate_book_percentage(
+    market_book: Dict[str, Any], use_market_book_objects: bool
+):
+    assert (
+        calculate_book_percentage(
+            MarketBook(**market_book) if use_market_book_objects else market_book,
+            Side.BACK,
+        )
+        == 1.0 / 1.98 + 1.0 / 1.98
+    )
+    assert (
+        calculate_book_percentage(
+            MarketBook(**market_book) if use_market_book_objects else market_book,
+            Side.LAY,
+        )
+        == 0
+    )
+
+    market_book["runners"][1]["ex"]["availableToBack"].pop()
+    market_book["runners"][1]["ex"]["availableToLay"].append({"price": 2.02, "size": 1})
+
+    assert (
+        calculate_book_percentage(
+            MarketBook(**market_book) if use_market_book_objects else market_book,
+            Side.BACK,
+        )
+        == 1.0 + 1.0 / 1.98
+    )
+
+
+def test_calculate_market_book_diff(market_book: Dict[str, Any]):
+    market_book_diff = calculate_market_book_diff(market_book, market_book)
+    for runner in market_book["runners"]:
+        for ex_key in EX_KEYS:
+            assert (
+                market_book_diff.get_size_changes(runner["selectionId"], ex_key) == {}
+            )
+
+    market_book_diff = calculate_market_book_diff(
+        MarketBook(**market_book), MarketBook(**market_book)
+    )
+    for runner in market_book["runners"]:
+        for ex_key in EX_KEYS:
+            assert (
+                market_book_diff.get_size_changes(runner["selectionId"], ex_key) == {}
+            )
+
+    previous_market_book = deepcopy(market_book)
+    market_book["runners"][1]["ex"]["availableToBack"].pop()
+    market_book_diff = calculate_market_book_diff(market_book, previous_market_book)
+    assert market_book_diff.get_size_changes(123, "availableToBack") == {}
+    assert market_book_diff.get_size_changes(123, "availableToLay") == {}
+    assert market_book_diff.get_size_changes(123, "tradedVolume") == {}
+    assert market_book_diff.get_size_changes(456, "availableToBack") == {1.98: -1}
+    assert market_book_diff.get_size_changes(456, "availableToLay") == {}
+    assert market_book_diff.get_size_changes(456, "tradedVolume") == {}
+
+    market_book_diff = calculate_market_book_diff(
+        MarketBook(**market_book), MarketBook(**previous_market_book)
+    )
+    assert market_book_diff.get_size_changes(123, "availableToBack") == {}
+    assert market_book_diff.get_size_changes(123, "availableToLay") == {}
+    assert market_book_diff.get_size_changes(123, "tradedVolume") == {}
+    assert market_book_diff.get_size_changes(456, "availableToBack") == {1.98: -1}
+    assert market_book_diff.get_size_changes(456, "availableToLay") == {}
+    assert market_book_diff.get_size_changes(456, "tradedVolume") == {}
+
+
+@pytest.mark.parametrize("use_market_book_objects", [False, True])
+def test_calculate_total_matched(
+    market_book: Dict[str, Any], use_market_book_objects: bool
+):
+    assert (
+        calculate_total_matched(
+            MarketBook(**market_book) if use_market_book_objects else market_book
+        )
+        == 0
+    )
+
+
+def test_does_market_book_contain_runner_names(market_book: Dict[str, Any]):
+    assert does_market_book_contain_runner_names(market_book)
+    assert not does_market_book_contain_runner_names(MarketBook(**market_book))
+    assert does_market_book_contain_runner_names(
+        MarketBook(**market_book, market_definition=market_book["marketDefinition"])
+    )
+
+
+def test_does_market_definition_contain_runner_names(market_definition: Dict[str, Any]):
+    assert does_market_definition_contain_runner_names(
+        MarketDefinition(**market_definition)
+    )
+    market_definition["runners"] = []
+    assert not does_market_definition_contain_runner_names(
+        MarketDefinition(**market_definition)
+    )
+
+
+def test_filter_runners(market_book: Dict[str, Any]):
+    assert (
+        len(
+            list(
+                filter_runners(market_book, status="REMOVED", excluded_selection_ids=[])
+            )
+        )
+        == 0
+    )
+    assert (
+        len(
+            list(
+                filter_runners(
+                    market_book, status="ACTIVE", excluded_selection_ids=[123]
+                )
+            )
+        )
+        == 1
+    )
+
+
+def test_get_runner_book_from_market_book(market_book: Dict[str, Any]):
+    with pytest.raises(ValueError):
+        get_runner_book_from_market_book(
+            market_book, selection_id=123, runner_name="foo"
+        )
+
+    with pytest.raises(TypeError):
+        get_runner_book_from_market_book(market_book, selection_id=123, return_type=int)
+
+    assert (
+        type(
+            get_runner_book_from_market_book(
+                MarketBook(**market_book), selection_id=123
+            )
+        )
+        is RunnerBook
+    )
+    assert get_runner_book_from_market_book(market_book, runner_name="alice") is None
+    assert (
+        get_runner_book_from_market_book(market_book, runner_name="bar")["selectionId"]
+        == 456
+    )
+
+
+def test_get_market_id_from_string():
+    assert (
+        get_market_id_from_string("/srv/betfair-prices/horse-racing/1.196842297.gz")
+        == "1.196842297"
+    )
+    assert (
+        get_market_id_from_string(
+            "/srv/betfair-prices/horse-racing/1.196842297.gz", as_integer=True
+        )
+        == 196842297
+    )
+    assert (
+        get_market_id_from_string("/srv/betfair-races/31323606.2355.jsonl.gz") is None
+    )
+
+
+def test_get_race_id_from_string():
+    assert (
+        get_race_id_from_string("/srv/betfair-races/31323606.2355.jsonl.gz")
+        == "31323606.2355"
+    )
+    assert (
+        get_race_id_from_string("/srv/betfair-prices/horse-racing/1.196842297.gz")
+        is None
+    )
+
+
+def test_get_selection_id_to_runner_name_map_from_market_catalogue(
+    market_catalogue: Dict[str, Any]
+):
+    assert get_selection_id_to_runner_name_map_from_market_catalogue(
+        market_catalogue
+    ) == {123: "foo", 456: "bar"}
+    assert get_selection_id_to_runner_name_map_from_market_catalogue(
+        MarketCatalogue(**market_catalogue)
+    ) == {123: "foo", 456: "bar"}
+
+
+def test_get_win_market_id_from_race_card(race_card: Dict[str, Any]):
+    assert get_win_market_id_from_race_card(race_card) == "1.456"
+    assert get_win_market_id_from_race_card(race_card, as_integer=True) == 456
+
+
+def test_is_market_book(
+    market_book: Dict[str, Any],
+    market_catalogue: Dict[str, Any],
+    race_card: Dict[str, Any],
+):
+    assert is_market_book(market_book)
+    assert not is_market_book(market_catalogue)
+    assert not is_market_book(race_card)
+
+    assert is_market_book(MarketBook(**market_book))
+    assert not is_market_book(MarketBook(**market_book).runners[0])
+
+
+def test_is_runner_book(
+    market_book: Dict[str, Any],
+    market_catalogue: Dict[str, Any],
+    race_card: Dict[str, Any],
+):
+    assert not is_runner_book(market_book)
+    assert not is_runner_book(market_catalogue)
+    assert not is_runner_book(race_card)
+
+    assert is_runner_book(market_book["runners"][0])
+    assert is_runner_book(MarketBook(**market_book).runners[0])
+
+
+def test_iterate_other_active_runners(market_book: Dict[str, Any]):
+    assert next(iterate_other_active_runners(market_book, 123))["selectionId"] == 456
+    assert next(iterate_other_active_runners(market_book, 456))["selectionId"] == 123
+
+
+def test_market_book_to_data_frame(market_book: Dict[str, Any]):
+    df = market_book_to_data_frame(
+        market_book, should_format_publish_time=True, should_output_runner_names=True
+    )
+    assert len(df) == 2
+    assert df["publish_time"].iloc[0] == "2022-04-03T14:00:00.000000Z"
+    assert df["publish_time"].iloc[1] == "2022-04-03T14:00:00.000000Z"
+    assert df["runner_name"].iloc[0] == "foo"
+    assert df["runner_name"].iloc[1] == "bar"
+
+    df = market_book_to_data_frame(
+        MarketBook(**market_book),
+        should_format_publish_time=True,
+        should_output_runner_names=True,
+    )
+    assert len(df) == 2
+    assert df["publish_time"].iloc[0] == "2022-04-03T14:00:00.000000Z"
+    assert df["publish_time"].iloc[1] == "2022-04-03T14:00:00.000000Z"
+    assert df["runner_name"].iloc[0] == "foo"
+    assert df["runner_name"].iloc[1] == "bar"
+
+
+def test_prices_file_to_csv_file(
+    market_definition: Dict[str, Any],
+    market_book: Dict[str, Any],
+    market_catalogue: Dict[str, Any],
+    tmp_path: Path,
+):
+    path_to_prices_file = tmp_path / f"1.123.json.gz"
+    with smart_open.open(path_to_prices_file, "w") as f:
+        f.write(
+            json.dumps(
+                {
+                    "op": "mcm",
+                    "clk": 0,
+                    "pt": market_book["publishTime"],
+                    "mc": [
+                        {
+                            "id": "1.123",
+                            "marketDefinition": market_definition,
+                            "rc": [
+                                {"id": 123, "atb": [[1.98, 1]]},
+                                {"id": 456, "atb": [[1.98, 1]]},
+                            ],
+                        }
+                    ],
+                }
+            )
+        )
+        f.write("\n")
+    path_to_csv_file = tmp_path / f"1.123.csv"
+    prices_file_to_csv_file(
+        path_to_prices_file,
+        path_to_csv_file,
+        should_output_runner_names=True,
+        should_format_publish_time=True,
+        should_output_market_types=True,
+        market_catalogues=[market_catalogue],
+    )
+    pd.testing.assert_frame_equal(
+        pd.read_csv(path_to_csv_file, dtype={"market_id": str, "handicap": "float64"}),
+        market_book_to_data_frame(
+            market_book,
+            should_output_runner_names=True,
+            should_format_publish_time=True,
+        ).assign(market_type="MATCH_ODDS"),
+    )
+
+
+def test_read_prices_file(
+    market_definition: Dict[str, Any],
+    market_book: Dict[str, Any],
+    market_catalogue: Dict[str, Any],
+    tmp_path: Path,
+):
+    del market_definition["runners"][0]["name"]
+    del market_definition["runners"][1]["name"]
+    path_to_prices_file = tmp_path / f"1.123.json.gz"
+    with smart_open.open(path_to_prices_file, "w") as f:
+        f.write(
+            json.dumps(
+                {
+                    "op": "mcm",
+                    "clk": 0,
+                    "pt": market_book["publishTime"],
+                    "mc": [
+                        {
+                            "id": "1.123",
+                            "marketDefinition": market_definition,
+                            "rc": [
+                                {"id": 123, "atb": [[1.98, 1]]},
+                                {"id": 456, "atb": [[1.98, 1]]},
+                            ],
+                        }
+                    ],
+                }
+            )
+        )
+        f.write("\n")
+    market_books = read_prices_file(
+        path_to_prices_file, market_catalogues=[market_catalogue]
+    )
+    assert len(market_books) == 1
+
+    market_books = read_prices_file(
+        path_to_prices_file, lightweight=False, market_catalogues=[market_catalogue]
+    )
+    assert len(market_books) == 1
+
+
+def test_read_race_file(tmp_path: Path):
+    path_to_race_file = tmp_path / "31323606.2355.jsonl.gz"
+    rc = {
+        "id": "31323606.2355",
+        "mid": "1.196610303",
+        "rrc": [
+            {
+                "ft": 1648165718800,
+                "id": 12883263,
+                "long": -95.5316312,
+                "lat": 29.9292254,
+                "spd": 0.11,
+                "prg": 1609.3,
+                "sfq": 0,
+            },
+            {
+                "ft": 1648165718800,
+                "id": 38368882,
+                "long": -95.5315912,
+                "lat": 29.9292075,
+                "spd": 0.16,
+                "prg": 1609.3,
+                "sfq": 0,
+            },
+            {
+                "ft": 1648165718800,
+                "id": 40957918,
+                "long": -95.5316203,
+                "lat": 29.9291875,
+                "spd": 0.11,
+                "prg": 1609.3,
+                "sfq": 0,
+            },
+            {
+                "ft": 1648165718800,
+                "id": 39605474,
+                "long": -95.5317173,
+                "lat": 29.9292874,
+                "spd": 0.86,
+                "prg": 1609.3,
+                "sfq": 0,
+            },
+            {
+                "ft": 1648165718800,
+                "id": 39605475,
+                "long": -95.5316561,
+                "lat": 29.9291837,
+                "spd": 1.14,
+                "prg": 1609.3,
+                "sfq": 0,
+            },
+        ],
+    }
+    with smart_open.open(path_to_race_file, "w") as f:
+        f.write(
+            json.dumps(
+                {
+                    "op": "rcm",
+                    "id": 2,
+                    "clk": "3097631015614646762",
+                    "pt": 1648165718972,
+                    "rc": [rc],
+                }
+            )
+        )
+        f.write("\n")
+    rcs = read_race_file(path_to_race_file)
+    assert len(rcs) == 1
+
+    del rcs[0]["pt"]
+    del rcs[0]["rpc"]
+    del rcs[0]["streaming_snap"]
+    del rcs[0]["streaming_unique_id"]
+    del rcs[0]["streaming_update"]
+
+    assert rcs[0] == rc
+
+
+def test_remove_bet_from_runner_book(market_book: Dict[str, Any]):
+    runner_book = market_book["runners"][0]
+    with pytest.raises(ValueError):
+        remove_bet_from_runner_book(
+            runner_book, price=1.98, size=2, available_side=Side.BACK
+        )
+    with pytest.raises(ValueError):
+        remove_bet_from_runner_book(
+            RunnerBook(**runner_book), price=1.98, size=2, available_side=Side.BACK
+        )
+
+    new_runner_book = remove_bet_from_runner_book(
+        runner_book, price=1.98, size=1, available_side=Side.BACK
+    )
+    assert len(new_runner_book["ex"]["availableToBack"]) == 0
+
+    new_runner_book = remove_bet_from_runner_book(
+        RunnerBook(**runner_book), price=1.98, size=1, available_side=Side.BACK
+    )
+    assert len(new_runner_book.ex.available_to_back) == 0
+
+
+def test_random_from_market_id():
+    with pytest.raises(ValueError):
+        random_from_market_id(-1)
+
+    with pytest.raises(ValueError):
+        random_from_market_id(0.5)
+
+    assert random_from_market_id("1.123") == 0.5181806162370606
---
+++ tests/test_prices.py
@@ -0,0 +1,226 @@
+from typing import Any, Dict
+
+import pytest
+from betfairlightweight.resources import RunnerBook
+
+from betfairutil import BETFAIR_PRICES
+from betfairutil import BETFAIR_PRICE_TO_PRICE_INDEX_MAP
+from betfairutil import decrement_price
+from betfairutil import get_inside_best_price
+from betfairutil import get_outside_best_price
+from betfairutil import get_spread
+from betfairutil import increment_price
+from betfairutil import is_price_the_same_or_better
+from betfairutil import is_price_worse
+from betfairutil import make_price_betfair_valid
+from betfairutil import Side
+
+
+@pytest.fixture
+def runner():
+    return {
+        "selectionId": 123,
+        "status": "ACTIVE",
+        "handicap": 0.0,
+        "ex": {"availableToBack": [], "availableToLay": [], "tradedVolume": []},
+    }
+
+
+def test_increment_price():
+    assert increment_price(1000) is None
+
+    for price in BETFAIR_PRICES:
+        if price == 1000:
+            continue
+        next_price = increment_price(price)
+        assert (
+            BETFAIR_PRICE_TO_PRICE_INDEX_MAP[next_price]
+            == BETFAIR_PRICE_TO_PRICE_INDEX_MAP[price] + 1
+        )
+
+
+def test_decrement_price():
+    assert decrement_price(1.01) is None
+
+    for price in BETFAIR_PRICES:
+        if price == 1.01:
+            continue
+        prev_price = decrement_price(price)
+        assert (
+            BETFAIR_PRICE_TO_PRICE_INDEX_MAP[prev_price]
+            == BETFAIR_PRICE_TO_PRICE_INDEX_MAP[price] - 1
+        )
+
+
+@pytest.mark.parametrize("use_runner_book_objects", [False, True])
+def test_get_spread(runner: Dict[str, Any], use_runner_book_objects: bool):
+    assert (
+        get_spread(RunnerBook(**runner) if use_runner_book_objects else runner) is None
+    )
+
+    runner["ex"]["availableToBack"].append({"price": 1.01, "size": 1})
+    assert (
+        get_spread(RunnerBook(**runner) if use_runner_book_objects else runner) is None
+    )
+
+    runner["ex"]["availableToLay"].append({"size": 1})
+    for i, price in enumerate(BETFAIR_PRICES):
+        if price == 1.01:
+            continue
+        runner["ex"]["availableToLay"][0]["price"] = price
+        assert (
+            get_spread(RunnerBook(**runner) if use_runner_book_objects else runner) == i
+        )
+
+
+@pytest.mark.parametrize("use_runner_book_objects", [False, True])
+def test_get_inside_best_price(runner: Dict[str, Any], use_runner_book_objects: bool):
+    assert (
+        get_inside_best_price(
+            RunnerBook(**runner) if use_runner_book_objects else runner, Side.BACK
+        )
+        is None
+    )
+    assert (
+        get_inside_best_price(
+            RunnerBook(**runner) if use_runner_book_objects else runner, Side.LAY
+        )
+        is None
+    )
+
+    runner["ex"]["availableToBack"].append({"price": 1.01, "size": 1})
+    assert (
+        get_inside_best_price(
+            RunnerBook(**runner) if use_runner_book_objects else runner, Side.BACK
+        )
+        == 1.02
+    )
+    assert (
+        get_inside_best_price(
+            RunnerBook(**runner) if use_runner_book_objects else runner, Side.LAY
+        )
+        is None
+    )
+
+    for i, price in enumerate(BETFAIR_PRICES):
+        if price == 1.01:
+            continue
+        runner["ex"]["availableToBack"][0]["price"] = price
+        inside_best_price = get_inside_best_price(
+            RunnerBook(**runner) if use_runner_book_objects else runner, Side.BACK
+        )
+        if price == 1000:
+            assert inside_best_price is None
+        else:
+            assert (
+                BETFAIR_PRICE_TO_PRICE_INDEX_MAP[price] + 1
+                == BETFAIR_PRICE_TO_PRICE_INDEX_MAP[inside_best_price]
+            )
+        assert (
+            get_inside_best_price(
+                RunnerBook(**runner) if use_runner_book_objects else runner, Side.LAY
+            )
+            is None
+        )
+
+
+@pytest.mark.parametrize("use_runner_book_objects", [False, True])
+def test_get_outside_best_price(runner: Dict[str, Any], use_runner_book_objects: bool):
+    assert (
+        get_outside_best_price(
+            RunnerBook(**runner) if use_runner_book_objects else runner, Side.BACK
+        )
+        is None
+    )
+    assert (
+        get_outside_best_price(
+            RunnerBook(**runner) if use_runner_book_objects else runner, Side.LAY
+        )
+        is None
+    )
+
+    runner["ex"]["availableToBack"].append({"price": 1000, "size": 1})
+    assert (
+        get_outside_best_price(
+            RunnerBook(**runner) if use_runner_book_objects else runner, Side.BACK
+        )
+        == 990
+    )
+    assert (
+        get_outside_best_price(
+            RunnerBook(**runner) if use_runner_book_objects else runner, Side.LAY
+        )
+        is None
+    )
+
+    for i, price in enumerate(BETFAIR_PRICES):
+        if price == 1000:
+            continue
+        runner["ex"]["availableToBack"][0]["price"] = price
+        outside_best_price = get_outside_best_price(
+            RunnerBook(**runner) if use_runner_book_objects else runner, Side.BACK
+        )
+        if price == 1.01:
+            assert outside_best_price is None
+        else:
+            assert (
+                BETFAIR_PRICE_TO_PRICE_INDEX_MAP[price] - 1
+                == BETFAIR_PRICE_TO_PRICE_INDEX_MAP[outside_best_price]
+            )
+        assert (
+            get_outside_best_price(
+                RunnerBook(**runner) if use_runner_book_objects else runner, Side.LAY
+            )
+            is None
+        )
+
+
+def test_is_price_the_same_or_better():
+    with pytest.raises(TypeError):
+        is_price_the_same_or_better(1.01, 1.02, "foo")
+
+    for price in BETFAIR_PRICES:
+        if price == 1000:
+            continue
+        next_price = increment_price(price)
+        assert is_price_the_same_or_better(price, price, Side.BACK)
+        assert not is_price_the_same_or_better(price, next_price, Side.BACK)
+        assert is_price_the_same_or_better(next_price, price, Side.BACK)
+
+        assert is_price_the_same_or_better(price, price, Side.LAY)
+        assert is_price_the_same_or_better(price, next_price, Side.LAY)
+        assert not is_price_the_same_or_better(next_price, price, Side.LAY)
+
+
+def test_is_price_worse():
+    with pytest.raises(TypeError):
+        is_price_the_same_or_better(1.01, 1.02, "foo")
+
+    for price in BETFAIR_PRICES:
+        if price == 1000:
+            continue
+        next_price = increment_price(price)
+        assert not is_price_worse(price, price, Side.BACK)
+        assert is_price_worse(price, next_price, Side.BACK)
+        assert not is_price_worse(next_price, price, Side.BACK)
+
+        assert not is_price_worse(price, price, Side.LAY)
+        assert not is_price_worse(price, next_price, Side.LAY)
+        assert is_price_worse(next_price, price, Side.LAY)
+
+
+def test_make_price_betfair_valid():
+    with pytest.raises(TypeError):
+        make_price_betfair_valid(2.0, "foo")
+
+    assert make_price_betfair_valid(1.005, Side.LAY) is None
+    assert make_price_betfair_valid(2000, Side.BACK) is None
+
+    for price in BETFAIR_PRICES:
+        prev_price = decrement_price(price)
+        next_price = increment_price(price)
+
+        if price != 1000:
+            assert make_price_betfair_valid(price + 0.001, Side.BACK) == next_price
+        if price != 1.01:
+            assert make_price_betfair_valid(price - 0.001, Side.LAY) == prev_price
