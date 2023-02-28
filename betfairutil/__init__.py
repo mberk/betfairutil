682d236b182b1e9a168cf9591b37ee3540bad6ed
jorgegarcia7 <jgarciam95@gmail.com>
2023-02-28 08:33:41 +0100

    Add calculate_available_volume function

--- betfairutil/__init__.py
+++ betfairutil/__init__.py
@@ -1540,6 +1540,37 @@ def calculate_book_percentage(
     return sum(implied_probabilities)
 
 
+def calculate_available_volume(
+    market_book: Union[Dict[str, Any], MarketBook],
+    side: Side,
+    max_book_percentage: float,
+) -> float:
+    """
+    Calculate the available volume up to a maximum book_percentage value.
+
+    :param market_book: A market book either as a dictionary or betfairlightweight MarketBook object
+    :param side: Indicate whether to get the available volume on the back or lay side.
+    :param max_book_percentage: Maximum book_percentage value to use for calculating the volume.
+    :return: Available total volume.
+    """
+    available_volume = 0
+    for depth in itertools.count():
+        book_percentage = 0
+        size = 0
+        for runner in market_book["runners"]:
+            runner_price_size = get_price_size_by_depth(
+                runner=runner, side=side, depth=depth
+            )
+            if runner_price_size:
+                book_percentage += 1.0 / runner_price_size["price"]
+                size += runner_price_size["size"]
+            else:
+                return available_volume
+
+        if book_percentage <= max_book_percentage:
+            available_volume += size
+
+
 def calculate_market_book_diff(
     current_market_book: Union[Dict[str, Any], MarketBook],
     previous_market_book: Union[Dict[str, Any], MarketBook],
