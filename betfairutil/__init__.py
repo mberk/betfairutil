1dbee8045afac9bad4e7392dc66830d94b1d5566
Maurice Berk <maurice@mauriceberk.com>
2023-04-19 11:25:50 +0100

    Add filter_suspended argument to get_last_pre_event_market_book_from_prices_file

--- betfairutil/__init__.py
+++ betfairutil/__init__.py
@@ -2123,21 +2123,30 @@ def get_first_market_definition_from_prices_file(
 
 
 def get_last_pre_event_market_book_from_prices_file(
-    path_to_prices_file: Union[str, Path]
+    path_to_prices_file: Union[str, Path], filter_suspended: bool = True
 ) -> Optional[Dict[str, Any]]:
+    """
+    Search a prices file for the last market book before the market turned in play
+
+    :param path_to_prices_file: The prices file to search
+    :param filter_suspended: Optionally ignore any pre-event market books where the market status is SUSPENDED
+    :return: The last pre-event market book, where the status is not SUSPENDED if filter_suspended has been set to True, provided one such market book exists in the prices file. If not then None will be returned
+    """
     g = create_market_book_generator_from_prices_file(path_to_prices_file)
     pre_event_market_book = None
     for market_book in g:
         if market_book["inplay"]:
             return pre_event_market_book
-        pre_event_market_book = market_book
+        if not filter_suspended or market_book["status"] != "SUSPENDED":
+            pre_event_market_book = market_book
 
 
 def get_pre_event_volume_traded_from_prices_file(
     path_to_prices_file: Union[str, Path],
 ) -> Optional[Union[int, float]]:
     pre_event_market_book = get_last_pre_event_market_book_from_prices_file(
-        path_to_prices_file
+        path_to_prices_file,
+        filter_suspended=False,
     )
     if pre_event_market_book is not None:
         pre_event_volume_traded = calculate_total_matched(pre_event_market_book)
