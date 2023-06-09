90e79e0dcd348812283509dae98cdf469c1e530f
Maurice Berk <maurice@mauriceberk.com>
2023-06-09 13:47:00 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ tests_require = ["pytest"]
 
 setup(
     name="betfairutil",
-    version="0.6.1",
+    version="0.6.2",
     description="Utility functions for working with Betfair data",
     long_description=long_description,
     long_description_content_type="text/markdown",
